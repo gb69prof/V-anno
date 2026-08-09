316749ec56b30cd4ec26ea2dfabf69ffbe3679c0
gb69prof <gb69prof@users.noreply.github.com>
2026-08-09 12:54:07 +0200

    Add Pasolini PWA sw.js

---
+++ Letteratura/Pasolini/sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'pasolini-gbprof-v1';
+const ASSETS = [
+  './','./index.html','./styles.css','./content.js','./app.js','./manifest.webmanifest','./icons/icon.svg',
+  './maps/01-mondo.svg','./maps/02-fratture.svg','./maps/03-immagine.svg','./maps/04-poetica.svg','./maps/05-opere.svg','./maps/06-conclusione.svg'
+];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
