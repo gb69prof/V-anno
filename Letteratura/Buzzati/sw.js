d509add8872d3d2845e855e2eed7868572c0e90b
gb69prof <gb69prof@users.noreply.github.com>
2026-08-10 19:52:48 +0200

    Aggiunge Buzzati: sw.js

---
+++ Letteratura/Buzzati/sw.js
@@ -0,0 +1,14 @@
+const CACHE = "buzzati-gbprof-v1";
+const ASSETS = [
+  "./", "./index.html", "./styles.css", "./content.js", "./app.js", "./manifest.webmanifest", "./icons/icon.svg",
+  "./maps/01-mondo.svg", "./maps/02-fratture.svg", "./maps/03-immagine.svg", "./maps/04-poetica.svg", "./maps/05-opere.svg", "./maps/06-conclusione.svg"
+];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
